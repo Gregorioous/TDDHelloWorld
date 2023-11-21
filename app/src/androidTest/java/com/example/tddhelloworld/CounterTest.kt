eef6a4280e7ad3d88d31628f1d69034393aa86a2
EgorKholod <veh@kvartel.com>
2023-11-21 16:44:02 +0300

    test is successful

---
+++ app/src/androidTest/java/com/example/tddhelloworld/CounterTest.kt
@@ -0,0 +1,32 @@
+package com.example.tddhelloworld
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class CounterTest {
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    @Test
+    fun test() {
+        onView(withId(R.id.textView)).check(matches(withText("0")))
+
+        onView(withId(R.id.button2)).perform(click())
+        onView(withId(R.id.textView)).check(matches(withText("1")))
+
+        onView(withId(R.id.button2)).perform(click())
+        onView(withId(R.id.textView)).check(matches(withText("2")))
+
+
+    }
+}
--- app/src/main/java/com/example/tddhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/tddhelloworld/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.tddhelloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tddhelloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+    var state:Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+            viewBinding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(viewBinding.root)
+
+        viewBinding.textView.text = state.toString()
+
+        viewBinding.button2.setOnClickListener{
+            state++
+            viewBinding.textView.text = state.toString()
+        }
+
     }
 }
